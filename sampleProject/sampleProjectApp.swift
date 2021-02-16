b83c8fac928e798e651b46fe4f3cb71d0b7c15ea
Guillem Fernandez <hello@guillem.cat>
2021-02-16 20:15:19 +0100

    Initial Commit

---
+++ sampleProject/sampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleProjectApp.swift
+//  sampleProject
+//
+//  Created by Guillermo Fernandez on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct sampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
